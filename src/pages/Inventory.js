8527584920f16561806eb23df5641418712cf6ce
Rico98 <asd.cqd98@gmail.com>
2022-11-21 23:57:35 -0500

    Updated Inventory.js

---
+++ src/pages/Inventory.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Inventory() {
+  return (
+    <div>
+      <h1>Inventory Page</h1>
+      <h2>TODO list:</h2>
+      <ul>
+        <li>Add react-table dependencies</li>
+        <li>Create connection to products db</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Inventory
